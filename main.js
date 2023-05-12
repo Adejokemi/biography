5be614741046c293635378a566a0a7ab786baca7
Adejokemi <omitogunsherifat75@gmail.com>
2023-05-12 08:37:44 +0100

    biography

---
+++ main.js
@@ -0,0 +1,5 @@
+const updateButton = document.querySelector('#update-button');
+updateButton.addEventListener('click', function() {
+  const learningText = document.querySelector('#learning-text');
+  learningText.textContent = "I am currently learning JavaScript";
+});
